df3dc26bb32ba4be356e09430b1bb221dfe827e9
Manuela Trindade <Manuelatrindade100@gmail.com>
2024-12-22 20:32:06 -0300

    ex 59

--- ex059.py
+++ ex059.py
@@ -8,17 +8,38 @@
 [ 5 ] sair do programa
 Seu programa deverá realizar a operação solicitada em cada caso.'''
 
+from time import sleep as s
 n1=int(input('Primeiro valor: '))
 n2=int(input('Segundo valor: '))
-print('   [ 1 ] somar\n   [ 2 ] multiplicar\n   [ 3 ] maior\n   [ 4 ] novos números\n   [ 5 ] sair do programa')
+print('   [ 1 ] somar\n   [ 2 ] multiplicar\n   [ 3 ] maior\n   [ 4 ] novos números\n   [ 5 ] sair do programa') # ou 3 aspas
 usuario=int(input('>>>>>> Qual é a sua opção? '))
 while usuario!=5:
     if usuario==1:
         print('A soma entre {} + {} é {}'.format(n1,n2,n1+n2))
-        print('=-'*15)
+        print('=-'*15), s(3)
         print('   [ 1 ] somar\n   [ 2 ] multiplicar\n   [ 3 ] maior\n   [ 4 ] novos números\n   [ 5 ] sair do programa')
         usuario=int(input('>>>>>> Qual é a sua opção? '))
     if usuario==2:
         print('O resultado de {} x {} é {}'.format(n1,n2,n1*n2))
-
-
+        print('=-'*15), s(3)
+        print('   [ 1 ] somar\n   [ 2 ] multiplicar\n   [ 3 ] maior\n   [ 4 ] novos números\n   [ 5 ] sair do programa')
+        usuario=int(input('>>>>>> Qual é a sua opção? '))
+    if usuario ==3:
+        print(f'Entre {n1} e {n2}, o maior valor é {max(n1,n2)}')
+        print('=-'*15), s(3)
+        print('   [ 1 ] somar\n   [ 2 ] multiplicar\n   [ 3 ] maior\n   [ 4 ] novos números\n   [ 5 ] sair do programa')
+        usuario=int(input('>>>>>> Qual é a sua opção? '))
+    if usuario==4:
+        n1=int(input(('Informe os números novamente:\nPrimeiro valor: ')))
+        n2=int(input('Segundo valor: '))
+        print('=-'*15), s(3)
+        print('   [ 1 ] somar\n   [ 2 ] multiplicar\n   [ 3 ] maior\n   [ 4 ] novos números\n   [ 5 ] sair do programa')
+        usuario=int(input('>>>>>> Qual é a sua opção? '))
+    if usuario>5:
+        print('Opção Invállida. Tente novamente')
+        print('=-'*15), s(3)
+        print('   [ 1 ] somar\n   [ 2 ] multiplicar\n   [ 3 ] maior\n   [ 4 ] novos números\n   [ 5 ] sair do programa')
+        usuario=int(input('>>>>>> Qual é a sua opção? '))
+print('Finalizando...')
+print('=-'*15), s(3)
+print('Fim do programa! Volte sempre!')
---
+++ ex060.py
@@ -0,0 +1,4 @@
+import math
+num=int(input('Digite um número para\nCalcular seu fatorial:'))
+print(f'Calculando {num}! = ')
+print(math.factorial(num))
