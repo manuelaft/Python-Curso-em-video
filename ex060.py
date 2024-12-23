fe12c3d83c33113dfbf1a57f98fd75f689c7cc74
Manuela Trindade <Manuelatrindade100@gmail.com>
2024-12-23 17:00:07 -0300

    ex 060

--- ex060.py
+++ ex060.py
@@ -1,4 +1,12 @@
+# ex060
+
+''' Faça um programa que leia um número qualquer e mostre o seu fatorial.
+
+Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120'''
+
 import math
-num=int(input('Digite um número para\nCalcular seu fatorial:'))
-print(f'Calculando {num}! = ')
-print(math.factorial(num))
+num=int(input('Digite um número para\nCalcular seu fatorial: '))
+print(f'Calculando {num}! = ',end='')
+for c in range (num,1,-1):
+    print(c,end=' x ')
+print(f'1 = {math.factorial(num)}')
