bd69d64c2cc32f23e820fe10fa7011fbea09dd7c
Manuela Trindade <Manuelatrindade100@gmail.com>
2025-01-03 15:28:39 -0300

    ex062

--- ex062.py
+++ ex062.py
@@ -3,20 +3,22 @@
 '''Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando ele disser que quer mostrar 0 termos.'''
 
 print('=-'*20)
-print('Gerador de PA')
+print('GERADOR DE PA')
 print('=-'*20)
 num=int(input('Primeiro termo: '))
 r=int(input('Razão da PA: '))
-c=0
-mais=int(10)
-while mais!=0:
-    while c<10:
+c=1
+mais=10
+total=0
+while mais !=0:
+    total=total+mais
+    mais+=1
+    while c<=total:
         c+=1
         num+=r
         print(num-r, end=' ➞  ')
     print('PAUSA')
-    mais=input('Quantos termos a mais você quer mostrar? ')
-    while mais<10:
-        mais+=r
-        print(mais)
+    mais=int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados.'.format(total))
+
 
