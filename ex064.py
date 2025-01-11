e3a3897db8f7a8872a02ebdd5bee72655ff9cfa9
Manuela Trindade <Manuelatrindade100@gmail.com>
2025-01-11 14:47:43 -0300

    ex065

--- ex064.py
+++ ex064.py
@@ -3,12 +3,15 @@
 '''Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. 
 No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).'''
 
-c=0
-num=0
+c=num=0
 lista=[] # a lista vazia precisa ser criada antes pq senão a cada loop ia criar uma lista NOVA, não atualizar os valores (print --> 999)
 while num!=999:
     num=int(input('Digite um número [999 para parar]: '))
     c+=1
-    lista.append(num)
+    lista.append(num) #append adiciona o elemento ao final da lista
 soma=sum(lista)
 print('Você digitou {} números e a soma entre eles foi {}'.format(c-1,soma-999))
+
+# Flag é condição de parada do while 
+
+'''tem como desconsiderar o flag sem precisar subtrair o 999 e o 1 do c, colocando o num(input) fora e dentro do while'''
