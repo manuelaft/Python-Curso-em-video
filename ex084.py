68ce9cadd8e617c39f22864ea0308ee9670771c5
Manuela Trindade <Manuelatrindade100@gmail.com>
2025-03-11 15:36:39 -0300

    ex084

--- ex084.py
+++ ex084.py
@@ -1,4 +1,40 @@
 # ex084
-dados=[]
+
+'''Faça um programa que leia nome e peso de várias pessoas,guardando tudo em uma lista. No final, mostre:                                                                                                   
+A) Quantas pessoas foram cadastradas.                                                                                                                
+    B) Uma listagem com as pessoas mais pesadas.                                                                                                    
+        C) Uma listagem com as pessoas mais leves.'''
+
+pessoas=[]
+maior_peso=0
+menor_peso=float('inf')
+print('=-'*30)
 while True:
-    dados.append(str(input('Nome: ')))
+    nomes=str(input('Nome: '))
+    pesos=int(input('Peso: '))
+    pessoas.append([nomes, pesos])
+
+    if pesos>=maior_peso:
+        maior_peso=pesos
+
+    if pesos<menor_peso:
+        menor_peso=pesos
+    
+    resp=' '
+    while resp not in 'SN':
+        resp=str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+    if resp in 'N':
+        break
+
+print('=-'*30)
+print('Ao todo você cadastrou {} pessoas.'.format(len(pessoas)))
+
+print(f'O maior peso foi {maior_peso}Kg. Peso de ',end='')
+for pessoa in pessoas:
+    if pessoa[1]==maior_peso:
+        print(pessoa[0],end=' ')
+
+print(f'\nO menor peso foi {menor_peso}Kg. Peso de ',end='')
+for pessoa in pessoas:
+    if pessoa[1]==menor_peso:
+        print(pessoa[0],end=' ')
